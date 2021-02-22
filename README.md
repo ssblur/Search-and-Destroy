9f19b7745d5260f448c5684c4fa99c11ddef0fe4
Patrick Emery <patrick.emery@outlook.com>
2021-02-21 23:02:49 -0500

    Initial Commit
    Signed-off-by: Patrick Emery <patrick.emery@outlook.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# Search and Destroy
+
+Made in a few minutes as a joke.
+
+To set up, insert the sound to play on join into this folder as sound.mp3, then fill in the info in config.json.
+
+You can get the Discord Token from your Discord Developer Portal after creating an application and a bot for the application.
+
+You can get the Guild ID by right clicking on a Guild and clicking "Copy ID" after turning on Discord Developer Mode.
+
+You can get a User's Snowflake by right clicking on a User anc clicking "Copy ID" after turning on Discord Developer Mode.
---
+++ index.js
@@ -0,0 +1,56 @@
+const discord = require('discord.js');
+const config = require('./config.json');
+const path = require('path');
+const fileSystem = require('fs');
+
+const client = new discord.Client();
+
+const delay = 250;
+
+var timer;
+
+client.once('ready', async function(){
+    console.log('Discord bot is ready.');
+    console.log('Loading...')
+    const guild = await this.guilds.fetch(config.guild);
+    const user = await guild.members.fetch(config.user);
+    console.log('Loaded.')
+
+    let tick, active, lastChannel;
+
+    active = async () => {
+        let channel = user.voice.channel;
+        if(channel) {
+            lastChannel = channel;
+            try {
+                await channel.join();
+            } catch (e) {}
+        } else {
+            clearInterval(timer)
+            timer = setInterval(tick, delay)
+            lastChannel.leave()
+        }
+    }
+
+    tick = async () => {
+        let channel = user.voice.channel;
+        if(channel) {
+            clearInterval(timer)
+            lastChannel = channel;
+            try {
+                let connection = await channel.join();
+                setTimeout(() => {
+                    console.log('Playing the sound clip...')
+                    connection.play(fileSystem.createReadStream('./sound.mp3'), {volume: 0.7})
+                }, 500);
+                timer = setInterval(active, delay);
+            } catch (e) {
+                timer = setInterval(tick, delay);
+            }
+
+        }
+    }
+    timer = setInterval(tick, delay);
+});
+
+client.login(config.token);
